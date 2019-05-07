3914d0d216276f9b7278a420024c6138d4be2478
Harm Boschloo <harm@boschloo.net>
2019-05-07 22:08:34 +0200

    added (de)serialization, refactored code

--- src/QuadTreeRaster.elm
+++ src/QuadTreeRaster.elm
@@ -1,33 +1,286 @@
 module QuadTreeRaster exposing
-    ( Raster
-    , get
-    , getSize
-    , init
-    , set
+    ( Raster, Size, init
+    , set, get, getSize
+    , Token(..), serialize, deserialize
     )
 
-import QuadTreeRaster.Internal as Internal exposing (Model, Size)
+{-|
+
+@docs Raster, Size, init
+@docs set, get, getSize
+@docs Token, serialize, deserialize
+
+-}
+
+import Bitwise
+import QuadTreeRaster.Internal as Internal
+    exposing
+        ( Model
+        , Node(..)
+        , Quads
+        , Raster(..)
+        )
 
 
-type Raster a
-    = Raster (Model a)
+type alias Raster a =
+    Internal.Raster a
 
 
+type alias Size =
+    { width : Int
+    , height : Int
+    }
+
+
+{-| -}
 init : Size -> a -> Raster a
 init size initialValue =
-    Raster (Internal.init size initialValue)
+    Raster
+        { size = size
+        , quadSize = initQuadSize size
+        , root = LeafNode initialValue
+        }
 
 
+initQuadSize : Size -> Int
+initQuadSize size =
+    max size.width size.height
+        |> toFloat
+        |> logBase 2
+        |> floor
+        |> (^) 2
+
+
+{-| -}
 getSize : Raster a -> Size
-getSize (Raster model) =
-    Internal.getSize model
+getSize (Raster { size }) =
+    size
 
 
+outOfBounds : Int -> Int -> Size -> Bool
+outOfBounds x y size =
+    x < 0 || y < 0 || x >= size.width || y >= size.height
+
+
+{-| -}
 set : Int -> Int -> a -> Raster a -> Raster a
-set x y value (Raster model) =
-    Raster (Internal.set x y value model)
+set x y value ((Raster model) as raster) =
+    if outOfBounds x y model.size then
+        raster
+
+    else
+        Raster
+            { size = model.size
+            , quadSize = model.quadSize
+            , root = updateValue x y value model.quadSize model.root
+            }
 
 
+{-| -}
 get : Int -> Int -> Raster a -> Maybe a
 get x y (Raster model) =
-    Internal.get x y model
+    if outOfBounds x y model.size then
+        Nothing
+
+    else
+        Just (getValue x y model.quadSize model.root)
+
+
+{-| FIXME tail-calls
+-}
+updateValue : Int -> Int -> a -> Int -> Node a -> Node a
+updateValue x y value quadSize node =
+    if quadSize == 0 then
+        LeafNode value
+
+    else
+        case node of
+            BranchNode quads ->
+                let
+                    newNode =
+                        updateQuadValue x y value quadSize quads
+                in
+                case newNode of
+                    BranchNode newQuads ->
+                        if
+                            (newQuads.q1 == LeafNode value)
+                                && (newQuads.q2 == LeafNode value)
+                                && (newQuads.q3 == LeafNode value)
+                                && (newQuads.q4 == LeafNode value)
+                        then
+                            LeafNode value
+
+                        else
+                            newNode
+
+                    LeafNode _ ->
+                        newNode
+
+            LeafNode leafValue ->
+                if value == leafValue then
+                    node
+
+                else
+                    updateQuadValue
+                        x
+                        y
+                        value
+                        quadSize
+                        { q1 = node
+                        , q2 = node
+                        , q3 = node
+                        , q4 = node
+                        }
+
+
+updateQuadValue : Int -> Int -> a -> Int -> Quads a -> Node a
+updateQuadValue x y value quadSize quads =
+    if x < quadSize then
+        if y < quadSize then
+            BranchNode
+                { q1 = updateValue x y value (Bitwise.shiftRightBy 1 quadSize) quads.q1
+                , q2 = quads.q2
+                , q3 = quads.q3
+                , q4 = quads.q4
+                }
+
+        else
+            BranchNode
+                { q1 = quads.q1
+                , q2 = quads.q2
+                , q3 = updateValue x (y - quadSize) value (Bitwise.shiftRightBy 1 quadSize) quads.q3
+                , q4 = quads.q4
+                }
+
+    else if y < quadSize then
+        BranchNode
+            { q1 = quads.q1
+            , q2 = updateValue (x - quadSize) y value (Bitwise.shiftRightBy 1 quadSize) quads.q2
+            , q3 = quads.q3
+            , q4 = quads.q4
+            }
+
+    else
+        BranchNode
+            { q1 = quads.q1
+            , q2 = quads.q2
+            , q3 = quads.q3
+            , q4 = updateValue (x - quadSize) (y - quadSize) value (Bitwise.shiftRightBy 1 quadSize) quads.q4
+            }
+
+
+getValue : Int -> Int -> Int -> Node a -> a
+getValue x y quadSize node =
+    case node of
+        BranchNode quads ->
+            if x < quadSize then
+                if y < quadSize then
+                    getValue x y (Bitwise.shiftRightBy 1 quadSize) quads.q1
+
+                else
+                    getValue x (y - quadSize) (Bitwise.shiftRightBy 1 quadSize) quads.q3
+
+            else if y < quadSize then
+                getValue (x - quadSize) y (Bitwise.shiftRightBy 1 quadSize) quads.q2
+
+            else
+                getValue (x - quadSize) (y - quadSize) (Bitwise.shiftRightBy 1 quadSize) quads.q4
+
+        LeafNode value ->
+            value
+
+
+
+-- SERIALIZATION --
+
+
+{-| -}
+type Token a
+    = Branch
+    | Leaf a
+
+
+type alias DeserializeHelp a =
+    { lastNodes : List (Node a)
+    , nextTokens : List (Token a)
+    }
+
+
+{-| -}
+serialize : Raster a -> List (Token a)
+serialize (Raster { root }) =
+    serializeHelp root [] []
+
+
+serializeHelp : Node a -> List (Node a) -> List (Token a) -> List (Token a)
+serializeHelp node nextNodes result =
+    case node of
+        BranchNode quads ->
+            serializeHelp quads.q1 (quads.q2 :: quads.q3 :: quads.q4 :: nextNodes) (Branch :: result)
+
+        LeafNode value ->
+            case nextNodes of
+                [] ->
+                    Leaf value :: result
+
+                nextNode :: otherNextNodes ->
+                    serializeHelp nextNode otherNextNodes (Leaf value :: result)
+
+
+{-| -}
+deserialize : Size -> List (Token a) -> Maybe (Raster a)
+deserialize size tokens =
+    let
+        { lastNodes } =
+            deserializeHelp
+                { lastNodes = []
+                , nextTokens = tokens
+                }
+    in
+    case lastNodes of
+        root :: [] ->
+            Just
+                (Raster
+                    { size = size
+                    , quadSize = initQuadSize size
+                    , root = root
+                    }
+                )
+
+        _ ->
+            Nothing
+
+
+deserializeHelp : DeserializeHelp a -> DeserializeHelp a
+deserializeHelp result =
+    case result.nextTokens of
+        [] ->
+            result
+
+        token :: remainingTokens ->
+            case token of
+                Branch ->
+                    case result.lastNodes of
+                        q1 :: q2 :: q3 :: q4 :: remainingNodes ->
+                            deserializeHelp
+                                { lastNodes =
+                                    BranchNode
+                                        { q1 = q1
+                                        , q2 = q2
+                                        , q3 = q3
+                                        , q4 = q4
+                                        }
+                                        :: remainingNodes
+                                , nextTokens = remainingTokens
+                                }
+
+                        _ ->
+                            { lastNodes = []
+                            , nextTokens = []
+                            }
+
+                Leaf value ->
+                    deserializeHelp
+                        { lastNodes = LeafNode value :: result.lastNodes
+                        , nextTokens = remainingTokens
+                        }
--- tests/QuadTreeRasterTest.elm
+++ tests/QuadTreeRasterTest.elm
@@ -1,7 +1,8 @@
 module QuadTreeRasterTest exposing (suite)
 
 import Expect
-import QuadTreeRaster.Internal as Internal exposing (Node(..), Size)
+import QuadTreeRaster as Raster exposing (Size, Token(..))
+import QuadTreeRaster.Internal exposing (Model, Node(..), Raster(..))
 import Test exposing (Test)
 
 
@@ -12,135 +13,203 @@ type Value
     | D
 
 
+getRoot : Raster a -> Node a
+getRoot (Raster model) =
+    model.root
+
+
 suite : Test
 suite =
     Test.describe "QuadTreeRaster"
         [ Test.test "init" <|
             \_ ->
-                Internal.init (Size 5 10) A
+                Raster.init (Size 5 10) A
                     |> Expect.equal
-                        { config =
-                            { width = 5
-                            , height = 10
+                        (Raster
+                            { size =
+                                { width = 5
+                                , height = 10
+                                }
                             , quadSize = 8
+                            , root = LeafNode A
                             }
-                        , values = Leaf A
+                        )
+        , Test.test "getSize" <|
+            \_ ->
+                Raster.init (Size 5 10) A
+                    |> Raster.getSize
+                    |> Expect.equal
+                        { width = 5
+                        , height = 10
                         }
         , Test.describe "get" <|
             let
-                model =
-                    { config =
-                        { width = 5
-                        , height = 10
-                        , quadSize = 8
-                        }
-                    , values =
-                        Branch
-                            { q1 = Leaf A
-                            , q2 = Leaf B
-                            , q3 = Leaf C
-                            , q4 = Leaf D
+                raster =
+                    Raster
+                        { size =
+                            { width = 5
+                            , height = 10
                             }
-                    }
+                        , quadSize = 8
+                        , root =
+                            BranchNode
+                                { q1 = LeafNode A
+                                , q2 = LeafNode B
+                                , q3 = LeafNode C
+                                , q4 = LeafNode D
+                                }
+                        }
             in
-            [ Test.test "q1 edge 0 0" (\_ -> Internal.get 0 0 model |> Expect.equal (Just A))
-            , Test.test "q1 edge 1 0" (\_ -> Internal.get 4 0 model |> Expect.equal (Just A))
-            , Test.test "q1 edge 0 1" (\_ -> Internal.get 0 7 model |> Expect.equal (Just A))
-            , Test.test "q1 edge 1 1" (\_ -> Internal.get 4 7 model |> Expect.equal (Just A))
-            , Test.test "q3 edge 0 0" (\_ -> Internal.get 0 8 model |> Expect.equal (Just C))
-            , Test.test "q3 edge 1 0" (\_ -> Internal.get 4 8 model |> Expect.equal (Just C))
-            , Test.test "q3 edge 0 1" (\_ -> Internal.get 0 9 model |> Expect.equal (Just C))
-            , Test.test "q3 edge 1 1" (\_ -> Internal.get 4 9 model |> Expect.equal (Just C))
-            , Test.test "out of bounds left" (\_ -> Internal.get -1 0 model |> Expect.equal Nothing)
-            , Test.test "out of bounds top" (\_ -> Internal.get 0 -1 model |> Expect.equal Nothing)
-            , Test.test "out of bounds right" (\_ -> Internal.get 5 0 model |> Expect.equal Nothing)
-            , Test.test "out of bounds bottom" (\_ -> Internal.get 0 10 model |> Expect.equal Nothing)
+            [ Test.test "q1 edge 0 0" (\_ -> Raster.get 0 0 raster |> Expect.equal (Just A))
+            , Test.test "q1 edge 1 0" (\_ -> Raster.get 4 0 raster |> Expect.equal (Just A))
+            , Test.test "q1 edge 0 1" (\_ -> Raster.get 0 7 raster |> Expect.equal (Just A))
+            , Test.test "q1 edge 1 1" (\_ -> Raster.get 4 7 raster |> Expect.equal (Just A))
+            , Test.test "q3 edge 0 0" (\_ -> Raster.get 0 8 raster |> Expect.equal (Just C))
+            , Test.test "q3 edge 1 0" (\_ -> Raster.get 4 8 raster |> Expect.equal (Just C))
+            , Test.test "q3 edge 0 1" (\_ -> Raster.get 0 9 raster |> Expect.equal (Just C))
+            , Test.test "q3 edge 1 1" (\_ -> Raster.get 4 9 raster |> Expect.equal (Just C))
+            , Test.test "out of bounds left" (\_ -> Raster.get -1 0 raster |> Expect.equal Nothing)
+            , Test.test "out of bounds top" (\_ -> Raster.get 0 -1 raster |> Expect.equal Nothing)
+            , Test.test "out of bounds right" (\_ -> Raster.get 5 0 raster |> Expect.equal Nothing)
+            , Test.test "out of bounds bottom" (\_ -> Raster.get 0 10 raster |> Expect.equal Nothing)
             ]
         , Test.describe "set" <|
             let
-                model =
-                    { config =
-                        { width = 12
-                        , height = 10
+                raster =
+                    Raster
+                        { size =
+                            { width = 12
+                            , height = 10
+                            }
                         , quadSize = 8
+                        , root = LeafNode A
                         }
-                    , values = Leaf A
-                    }
             in
             [ Test.test "same value leaf"
                 (\_ ->
-                    model
-                        |> Internal.set 0 0 A
-                        |> .values
-                        |> Expect.equal (Leaf A)
+                    raster
+                        |> Raster.set 0 0 A
+                        |> getRoot
+                        |> Expect.equal (LeafNode A)
                 )
             , Test.test "full branching all quads"
                 (\_ ->
-                    model
-                        |> Internal.set 5 3 B
-                        |> .values
+                    raster
+                        |> Raster.set 5 3 B
+                        |> getRoot
                         |> Expect.equal
-                            (Branch
+                            (BranchNode
                                 { q1 =
-                                    Branch
-                                        { q1 = Leaf A
+                                    BranchNode
+                                        { q1 = LeafNode A
                                         , q2 =
-                                            Branch
-                                                { q1 = Leaf A
-                                                , q2 = Leaf A
+                                            BranchNode
+                                                { q1 = LeafNode A
+                                                , q2 = LeafNode A
                                                 , q3 =
-                                                    Branch
-                                                        { q1 = Leaf A
-                                                        , q2 = Leaf A
-                                                        , q3 = Leaf A
-                                                        , q4 = Leaf B
+                                                    BranchNode
+                                                        { q1 = LeafNode A
+                                                        , q2 = LeafNode A
+                                                        , q3 = LeafNode A
+                                                        , q4 = LeafNode B
                                                         }
-                                                , q4 = Leaf A
+                                                , q4 = LeafNode A
                                                 }
-                                        , q3 = Leaf A
-                                        , q4 = Leaf A
+                                        , q3 = LeafNode A
+                                        , q4 = LeafNode A
                                         }
-                                , q2 = Leaf A
-                                , q3 = Leaf A
-                                , q4 = Leaf A
+                                , q2 = LeafNode A
+                                , q3 = LeafNode A
+                                , q4 = LeafNode A
                                 }
                             )
                 )
             , Test.test "collapse to root all quads"
                 (\_ ->
-                    model
-                        |> Internal.set 5 3 B
-                        |> Internal.set 5 3 A
-                        |> .values
-                        |> Expect.equal (Leaf A)
+                    raster
+                        |> Raster.set 5 3 B
+                        |> Raster.set 5 3 A
+                        |> getRoot
+                        |> Expect.equal (LeafNode A)
                 )
             , Test.test "out of bounds left"
                 (\_ ->
-                    model
-                        |> Internal.set -1 0 C
-                        |> .values
-                        |> Expect.equal (Leaf A)
+                    raster
+                        |> Raster.set -1 0 C
+                        |> getRoot
+                        |> Expect.equal (LeafNode A)
                 )
             , Test.test "out of bounds top"
                 (\_ ->
-                    model
-                        |> Internal.set 0 -1 C
-                        |> .values
-                        |> Expect.equal (Leaf A)
+                    raster
+                        |> Raster.set 0 -1 C
+                        |> getRoot
+                        |> Expect.equal (LeafNode A)
                 )
             , Test.test "out of bounds right"
                 (\_ ->
-                    model
-                        |> Internal.set 12 0 C
-                        |> .values
-                        |> Expect.equal (Leaf A)
+                    raster
+                        |> Raster.set 12 0 C
+                        |> getRoot
+                        |> Expect.equal (LeafNode A)
                 )
             , Test.test "out of bounds bottom"
                 (\_ ->
-                    model
-                        |> Internal.set 0 10 C
-                        |> .values
-                        |> Expect.equal (Leaf A)
+                    raster
+                        |> Raster.set 0 10 C
+                        |> getRoot
+                        |> Expect.equal (LeafNode A)
                 )
             ]
+        , Test.test "serialize" <|
+            \_ ->
+                Raster.init (Size 12 10) A
+                    |> Raster.set 5 3 B
+                    |> Raster.serialize
+                    |> Expect.equal
+                        [ Leaf A
+                        , Leaf A
+                        , Leaf A
+                        , Leaf A
+                        , Leaf A
+                        , Leaf A
+                        , Leaf B
+                        , Leaf A
+                        , Leaf A
+                        , Leaf A
+                        , Branch
+                        , Leaf A
+                        , Leaf A
+                        , Branch
+                        , Leaf A
+                        , Branch
+                        , Branch
+                        ]
+        , Test.test "deserialize" <|
+            \_ ->
+                [ Leaf A
+                , Leaf A
+                , Leaf A
+                , Leaf A
+                , Leaf A
+                , Leaf A
+                , Leaf B
+                , Leaf A
+                , Leaf A
+                , Leaf A
+                , Branch
+                , Leaf A
+                , Leaf A
+                , Branch
+                , Leaf A
+                , Branch
+                , Branch
+                ]
+                    |> Raster.deserialize (Size 12 10)
+                    |> Expect.equal
+                        (Just
+                            (Raster.init (Size 12 10) A
+                                |> Raster.set 5 3 B
+                            )
+                        )
         ]
