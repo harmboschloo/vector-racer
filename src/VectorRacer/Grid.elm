8911155727f05c6ee802fcbc68602540ffb0986b
Harm Boschloo <harm@boschloo.net>
2019-05-19 13:30:23 +0200

    added basic grid

---
+++ src/VectorRacer/Grid.elm
@@ -0,0 +1,31 @@
+module VectorRacer.Grid exposing (Grid, getAnchorPoint, getSpacing, init)
+
+import VectorRacer.Vector exposing (Position)
+
+
+type Grid
+    = Grid Model
+
+
+type alias Model =
+    { anchorPoint : Position
+    , spacing : Int
+    }
+
+
+init : Position -> Int -> Grid
+init anchorPoint spacing =
+    Grid
+        { anchorPoint = anchorPoint
+        , spacing = spacing
+        }
+
+
+getAnchorPoint : Grid -> Position
+getAnchorPoint (Grid model) =
+    model.anchorPoint
+
+
+getSpacing : Grid -> Int
+getSpacing (Grid model) =
+    model.spacing
